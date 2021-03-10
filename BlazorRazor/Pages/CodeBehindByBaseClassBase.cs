ca8f0f89c3a4b52f070b46b08e2af29b1a7a040c
k-green-panda <stvcx5c@gmail.com>
2021-03-10 16:05:53 +0900

    code behind approach to razor component

---
+++ BlazorRazor/Pages/CodeBehindByBaseClassBase.cs
@@ -0,0 +1,22 @@
+﻿using BlazorRazor.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorRazor.Pages
+{
+    // デザインクラスと異なるクラス名で定義 / ComponentBase継承
+    public class CodeBehindByBaseClassBase : ComponentBase
+    {
+        [Inject]
+        public WeatherForecastService ForecastService { get; set; }
+
+        // 子クラス(デザインクラス)から参照できるようアクセス修飾子を設定する
+        protected WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
---
+++ BlazorRazor/Pages/CodeBehindByPartialClass.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazorRazor.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorRazor.Pages
+{
+    // 部分クラスとしてコードビハインドクラスを定義する
+    public partial class CodeBehindByPartialClass
+    {
+        // サービスコンテナに登録したインスタンス受け取り(Inject属性)
+        [Inject]
+        public WeatherForecastService ForecastService { get; set; }
+
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
